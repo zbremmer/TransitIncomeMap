9413aea109411f4f144c8b3eb09d5173d854bfd7
zbremmer <bremmezm@gmail.com>
2016-03-11 09:03:51 -0800

    Added files via upload

---
+++ getDataJSON.R
@@ -0,0 +1,20 @@
+# Get census income data and convert to data frame
+
+#install.packages("RJSONIO")
+library(RJSONIO)
+
+#read in JSON
+rawData <- fromJSON("http://api.census.gov/data/2014/acs5/profile?get=DP03_0062E&for=zip+code+tabulation+area:*&key=faf52bc88216c93a18350a5dff192b7c17dc1a79")
+
+#Deal with any null values 
+incomeData <- lapply(rawData, function(x){x[sapply(x, is.null)]<-NA 
+              unlist(x)}) 
+
+#Create data frame and add header names from first row
+incomeData <- as.data.frame(do.call(rbind, incomeData))
+names(incomeData) = as.character(unlist(incomeData[1,]))
+incomeData <- incomeData[-1,]
+
+#Write to csv
+output <- data.frame(ZipCode = incomeData$`zip code tabulation area`, MedianIncome = incomeData$DP03_0062E)
+write.csv(output, file = 'Median Income by Zip.csv', row.names = FALSE)
